2975c549c5b525d90264d737e0b900bb76516928
TstXerox <jouahriadib+github@gmail.com>
2024-08-20 06:44:57 -0400

    Error handling

--- server.py
+++ server.py
@@ -8,7 +8,10 @@ def emo_detector():
     text = request.args.get('textToAnalyze')
     response = emotion_detector(text)
 
-    return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again."
+    else:
+        return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
 
 @app.route("/")
 def render_index_page():
